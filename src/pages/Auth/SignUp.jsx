1dfabe29fe8bc576e11983d644145824c9e04095
Hasindu Chandeepa <chandeepahasindu@gmail.com>
2025-06-10 15:29:03 +0530

    update auth finall

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
 import { Link, useNavigate } from 'react-router-dom';
 import Input from '../../components/inputs/input';
 import { validateEmail } from '../../utils/helper';
 import ProfilePhotoSector from '../../components/layouts/ProfilePhotoSector';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { userContext } from '../../context/userContext';
+import uploadImage from '../../utils/uploadImage';
 
 const SignUp = () => {
 
@@ -13,6 +17,8 @@ const SignUp = () => {
   const [password,setPassword] = useState("");
 
   const[error,setError] =useState(null);
+
+  const {updateUser} = useContext(userContext);
 
   const navigate = useNavigate();
 
@@ -34,6 +40,34 @@ const SignUp = () => {
       return;
     }
     setError("");
+
+    try {
+
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImgaeUrl = imgUploadRes.imageUrl || "";
+      }
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER,{
+        fullName,
+        email,
+        password,
+        profileImgaeUrl
+      });
+
+      const {token,user} =response.data;
+
+      if(token){
+        localStorage.setItem("token",token);
+        updateUser(user);
+        navigate("/dashboard")
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message)
+      }else{
+        setError("something went wrong.try again")
+      }
+    }
 
   };
 
